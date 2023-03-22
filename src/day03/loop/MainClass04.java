a22041de7717a9554ca8ae2e1b0c7f2d86b79f33
ITSC <ITSC@test.com>
2023-03-22 20:29:37 +0900

    숙제!

---
+++ src/day03/loop/MainClass04.java
@@ -0,0 +1,62 @@
+package day03.loop;
+/*
+
+*
+**
+***
+****
+*****
+******
+*******
+
+반복문 조건문 연습
+ 주말숙제 2중for문 코드 분석하기 
+ 
+2번
+   *
+  **
+ ***
+****
+
+3번
+   *
+  ***
+ *****
+******* 
+  
+4번
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+
+   ☆
+   *
+  ***
+ *****
+******* 
+  | | Merry Christmas~*
+
+
+
+ 
+ */
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 7; i++) {
+			System.out.print(i+": ");
+			for(int j = 0; j < i+1; j++) { 
+				System.out.print("*");
+			}
+			
+			System.out.println();	// 개행
+		}
+	}
+
+}
